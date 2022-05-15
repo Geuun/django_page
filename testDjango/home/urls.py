f91d0da5efc27ce32298de4177fd2cba5b86c4dd
KimJaeGeun <jgk9612@gmail.com>
2022-05-15 15:05:33 +0900

    feat: Add Study Django Files until 220515
    find back secret key tomorrow

--- testDjango/home/urls.py
+++ testDjango/home/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import include
 from . import views
 
 urlpatterns = [
-    path('', views.hello, name='hello'),
+    path('', views.hello, name='hello'), # testProject/urls.py 를 한 번 거쳐서 온 요청의 URL의 path가 빈 문자열일 경우 home/view.py 안의 hello 함수의 규칙을 따라라.
+    
 ]
--- testDjango/testProject/urls.py
+++ testDjango/testProject/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from django.conf.urls import include
 from django.views.generic import RedirectView
 
+# http://localhost
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),    # URL의 path가 'home/'일 경우 일단 home앱으로 가봐라.
-    path('', RedirectView.as_view(url='home/', permanent=True)),
+    path('', RedirectView.as_view(url='home/', permanent=True)),    #path에 공백을 넣게되면 자동으로 /home 으로 리다이렉트 되는 코드 따라서 http://127.0.0.1:8000/ 을 입력하더라도 http://127.0.0.1:8000/home/으로 이동시킨다.
+    
 ]
